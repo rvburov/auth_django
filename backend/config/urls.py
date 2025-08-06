dad15a209616744bd26022d63663123c3e2be9d1
Roman Burov <romanburov89@gmail.com>
2025-08-06 16:54:36 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Authentication URLs
+    path('auth/', include('authentication.jwt.urls')),
+    path('oauth/', include('authentication.oauth.urls')),
+    
+    # Users URLs
+    path('users/', include('users.urls')),
+    
+    # API Docs
+    path('docs/', include('docs.urls')),
+]
