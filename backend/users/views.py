85ee23b4fd2641d3d605e6410121851b3bcdc0ae
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:39:04 +0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,1 +1,14 @@
+from rest_framework import generics, permissions
+from .serializers import UserSerializer, RegisterSerializer
+from rest_framework.response import Response
 
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
