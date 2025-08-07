295b1688022932bc25cc6ce3ca66924214e41f40
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:34:08 +0300

    Update validators.py

--- backend/authentication/custom_auth/validators.py
+++ backend/authentication/custom_auth/validators.py
@@ -1,1 +1,12 @@
+from django.core.exceptions import ValidationError
+import re
 
+def validate_password_complexity(value):
+    if len(value) < 8:
+        raise ValidationError("Password must be at least 8 characters long")
+    if not re.search(r'[A-Z]', value):
+        raise ValidationError("Password must contain at least one uppercase letter")
+    if not re.search(r'[a-z]', value):
+        raise ValidationError("Password must contain at least one lowercase letter")
+    if not re.search(r'[0-9]', value):
+        raise ValidationError("Password must contain at least one digit")
