798bacf4688e4c295132a3ab2032884743213ed3
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:34:23 +0300

    Update urls.py

--- backend/authentication/custom_auth/urls.py
+++ backend/authentication/custom_auth/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, CustomPasswordResetView
 
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('password/reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+]
