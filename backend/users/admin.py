13f2da6f509118930b6b05c1bdf412cbed89ec5a
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:39:30 +0300

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_verified')
+    list_filter = ('is_staff', 'is_verified')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
