24f7d31a9d429353b460c8f390a5d7e558700991
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:38:18 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,1 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
