d4c1e2c8f282c73216dae9bc87759e7a9aa51313
Roman Burov <romanburov89@gmail.com>
2025-08-07 18:21:49 +0300

    Update urls.py

--- backend/authentication/oauth/urls.py
+++ backend/authentication/oauth/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from .views import GoogleOAuthView, OAuthCallbackView
 
+urlpatterns = [
+    path('google/', GoogleOAuthView.as_view(), name='google-auth'),
+    path('yandex/', GoogleOAuthView.as_view(), name='yandex-auth'),
+    path('callback/', OAuthCallbackView.as_view(), name='oauth-callback'),
+]
