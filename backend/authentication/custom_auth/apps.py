9bade7a73b75044b3b135715b1dc600ab306c490
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:00:44 +0300

    Update apps.py

--- backend/authentication/custom_auth/apps.py
+++ backend/authentication/custom_auth/apps.py
@@ -1,1 +1,6 @@
+from django.apps import AppConfig
 
+class CustomAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authentication.custom_auth'
+    verbose_name = 'Кастомная аутентификация'
