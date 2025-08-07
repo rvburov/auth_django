c66e1b94eebcc8473a2634236cac08d70ec03097
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:33:54 +0300

    Update serializers.py

--- backend/authentication/custom_auth/serializers.py
+++ backend/authentication/custom_auth/serializers.py
@@ -1,1 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .validators import validate_password_complexity
 
+User = get_user_model()
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, validators=[validate_password_complexity])
