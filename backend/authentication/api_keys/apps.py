e4714b5f81a7700f843c30277cccdd6dde593137
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:24:37 +0300

    Delete backend/authentication/api_keys directory

--- backend/authentication/api_keys/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class ApiKeysConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication.api_keys'
-    verbose_name = 'API Ключи'
