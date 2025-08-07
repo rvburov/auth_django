46b9fbfa8c7d20a94ae38288616eeceefaf3e899
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:36:32 +0300

    Update google.py

--- backend/authentication/oauth/providers/google.py
+++ backend/authentication/oauth/providers/google.py
@@ -1,1 +1,18 @@
+import requests
+from django.conf import settings
+from ..models import OAuthUser
 
+def get_google_user_info(access_token):
+    response = requests.get(
+        settings.OAUTH_PROVIDERS['google']['userinfo_url'],
+        params={'access_token': access_token}
+    )
+    if response.status_code == 200:
+        data = response.json()
+        return OAuthUser(
+            provider='google',
+            email=data.get('email'),
+            name=data.get('name'),
+            picture=data.get('picture')
+        )
+    return None
