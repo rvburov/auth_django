e8afe776788614b79105d72c8dd07e6e41bb1151
Roman Burov <romanburov89@gmail.com>
2025-08-07 18:27:13 +0300

    Update serializers.py

--- backend/authentication/oauth/serializers.py
+++ backend/authentication/oauth/serializers.py
@@ -1,1 +1,11 @@
+from rest_framework import serializers
 
+class OAuthCallbackSerializer(serializers.Serializer):
+    code = serializers.CharField(required=True)
+    state = serializers.CharField(required=False)
+    error = serializers.CharField(required=False)
+    
+    def validate(self, data):
+        if data.get('error'):
+            raise serializers.ValidationError(data['error'])
+        return data
