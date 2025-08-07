41f3a3faa78e7702f71ac51301f15c3616db839d
Roman Burov <romanburov89@gmail.com>
2025-08-07 18:28:46 +0300

    Update admin.py

--- backend/authentication/oauth/admin.py
+++ backend/authentication/oauth/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from .models import OAuthUser
 
+@admin.register(OAuthUser)
+class OAuthUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'provider', 'email', 'created_at')
+    list_filter = ('provider', 'created_at')
+    search_fields = ('user__email', 'email', 'provider_id')
+    readonly_fields = ('created_at',)
+    date_hierarchy = 'created_at'
