633613228aabe20eba2cfa31479a97683b5101da
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:38:46 +0300

    Update serializers.py

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,1 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'username', 'avatar', 'is_verified']
+        read_only_fields = ['is_verified']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
