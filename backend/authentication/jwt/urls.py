fc13d87c3ea7ede551421ce9ac8a31926754976e
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:35:40 +0300

    Update urls.py

--- backend/authentication/jwt/urls.py
+++ backend/authentication/jwt/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from .views import CustomTokenObtainPairView, CustomTokenRefreshView
 
+urlpatterns = [
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+]
