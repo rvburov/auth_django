b13b3a04fd92efa67e8d99a7e02016ed63e257c6
Roman Burov <romanburov89@gmail.com>
2025-08-07 17:36:16 +0300

    Update oauth_config.py

--- backend/authentication/oauth/oauth_config.py
+++ backend/authentication/oauth/oauth_config.py
@@ -1,1 +1,18 @@
-
+OAUTH_PROVIDERS = {
+    'google': {
+        'client_id': 'your-google-client-id',
+        'client_secret': 'your-google-secret',
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://oauth2.googleapis.com/token',
+        'userinfo_url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+        'scopes': ['openid', 'email', 'profile']
+    },
+    'yandex': {
+        'client_id': 'your-yandex-client-id',
+        'client_secret': 'your-yandex-secret',
+        'authorize_url': 'https://oauth.yandex.ru/authorize',
+        'token_url': 'https://oauth.yandex.ru/token',
+        'userinfo_url': 'https://login.yandex.ru/info',
+        'scopes': ['login:email', 'login:info']
+    }
+}
