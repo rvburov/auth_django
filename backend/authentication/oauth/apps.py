c9671ad2f6c77b2b6ccebcf1f953b5eb34dfdf06
Roman Burov <romanburov89@gmail.com>
2025-08-07 16:44:17 +0300

    Update apps.py

--- backend/authentication/oauth/apps.py
+++ backend/authentication/oauth/apps.py
@@ -1,1 +1,6 @@
+from django.apps import AppConfig
 
+class OAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authentication.oauth'
+    verbose_name = 'OAuth провайдеры'
